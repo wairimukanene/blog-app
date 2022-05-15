853107ca020a496c86d71c074b2466e0ede6713d
wairimu.Kanene <mercykanene254@gmail.com>
2022-05-15 05:09:43 +0300

    commit5-imported return render template and added route home.html

--- website/views.py
+++ website/views.py
@@ -1,6 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+
+views = Blueprint("views", __name__)
 
 
-views = Blueprint("views",__name__)
-
-
+@views.route("/")
+@views.route("/home")
+def home():
+    return render_template("home.html")
