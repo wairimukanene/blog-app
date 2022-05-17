655d473e10a24c5a252e4aa2406a72805cf5101d
wairimu.Kanene <mercykanene254@gmail.com>
2022-05-17 23:46:41 +0300

    commit21-added the password hashing function

--- app/models.py
+++ app/models.py
@@ -20,3 +20,20 @@ def load_user(user_id):
     blog = db.relationship('Blog', backref='user', lazy='dynamic')
     pass_secure = db.Column(db.String(255))
     comment = db.relationship('Comment', backref='user', lazy='dynamic')
+    
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    def __repr__(self):
+        return f'User{self.username}'
+    
+    
