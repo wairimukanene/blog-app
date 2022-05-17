d1dcfafd90d957cf39b06b0494b98564f2ed5b4c
wairimu.Kanene <mercykanene254@gmail.com>
2022-05-17 22:59:04 +0300

    added authentication routes and configuring models and custom validators

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,7 @@ from flask import render_template,
 from . import main
 from flask_login import login_required,current_user
 from ..models import User
+from .forms import UpdateProfile
 
 
 @main.route('/')
@@ -42,4 +43,22 @@ def profile(uname):
     title = f'{uname} Profile'
     
     return render_template("profile/profile.html", user = user, title = title)
+  
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+    title = 'Update | Profile'
+    return render_template('profile/update.html',form =form, title = title)
 
--- app/models.py
+++ app/models.py
@@ -8,3 +8,15 @@ from . import login_manager
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+  
+  class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    blog = db.relationship('Blog', backref='user', lazy='dynamic')
+    pass_secure = db.Column(db.String(255))
+    comment = db.relationship('Comment', backref='user', lazy='dynamic')
