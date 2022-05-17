2c27f09cf4346f267be439bd74b9b0c5f5aa1151
wairimu.Kanene <mercykanene254@gmail.com>
2022-05-17 22:15:42 +0300

    commit10-added app title

---
+++ app/main/views.py
@@ -0,0 +1,14 @@
+from flask import render_template
+from . import main
+
+
+@main.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    
+    title = 'Blogger'
+    
+    return render_template('index.html', title = title)
